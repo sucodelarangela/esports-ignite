fd719e767a3b8651c270134a7598961a93dc51a0
Angela Caldas <aes.caldas@gmail.com>
2022-09-13 20:13:19 -0300

    config get routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,33 +2,82 @@
 // const express = require('express');
 // Atualmente, podemos importar indo no arquivo package.json e incluindo a linha "type": "module" e alterando a extensão do arquivo server.js para server.mjs (se formos trabalhar com TS, podemos usar apenas a extensão .ts). Dessa forma, podemos importar o express dessa forma:
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 // Criando a aplicação
 const app = express();
 
-app.get('/games', (req, res) => {
-    return res.json([]);
+// conectando ao banco de dados
+const prisma = new PrismaClient();
+
+// get da lista de games
+app.get('/games', async (req, res) => {
+    // seleciona todos os jogos e conta quantos ads tem em cada
+    const games = await prisma.game.findMany({
+        include: {
+            _count: {
+                select: {
+                    ads: true
+                }
+            }
+        }
+    });
+
+    return res.json(games);
 });
 
 app.post('/ads', (req, res) => {
     return res.status(201).json([]);
 });
 
-app.get('/games/:id/ads', (req, res) => {
-    // const gameId = req.params.id;
+// get dos anúncios por jogo
+app.get('/games/:id/ads', async (req, res) => {
+    const gameId = req.params.id;
 
-    return res.json([
-        { id: 1, name: 'Anúncio 1' },
-        { id: 2, name: 'Anúncio 2' },
-        { id: 3, name: 'Anúncio 3' },
-        { id: 4, name: 'Anúncio 4' }
-    ]);
+    // seleciona um jogo que tenha o mesmo id dos params da url, em ordem decrescente, e retorna todos os campos do banco de dados exceto o discord
+    const ads = await prisma.ad.findMany({
+        select: {
+            id: true,
+            name: true,
+            weekDays: true,
+            useVoiceChannel: true,
+            yearsPlaying: true,
+            hourStart: true,
+            hourEnd: true
+        },
+        where: {
+            gameId
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+
+    // tratando os dados de weekDays( no db está salvo como string, aqui criaremos um array com split)
+    return res.json(ads.map(ad => {
+        return {
+            ...ad,
+            weekDays: ad.weekDays.split(',')
+        };
+    }));
 });
 
-app.get('/ads/:id/discord', (req, res) => {
-    // const adId = req.params.id;
+// get do discord de acordo com o id do anúncio
+app.get('/ads/:id/discord', async (req, res) => {
+    const adId = req.params.id;
 
-    return res.json([]);
+    const ad = await prisma.ad.findUniqueOrThrow({
+        select: {
+            discord: true
+        },
+        where: {
+            id: adId
+        }
+    });
+
+    return res.json({
+        discord: ad.discord
+    });
 });
 
 app.listen(3333);
