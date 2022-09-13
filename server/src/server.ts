f419e6dcd39c76fff7c779670cf1d9ba44fd31db
Angela Caldas <aes.caldas@gmail.com>
2022-09-13 16:51:47 -0300

    prisma and sqlite setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,14 +6,29 @@ import express from 'express';
 // Criando a aplicação
 const app = express();
 
-// Criando a primeira rota
-app.get('/ads', (req, res) => {
+app.get('/games', (req, res) => {
+    return res.json([]);
+});
+
+app.post('/ads', (req, res) => {
+    return res.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (req, res) => {
+    // const gameId = req.params.id;
+
     return res.json([
         { id: 1, name: 'Anúncio 1' },
         { id: 2, name: 'Anúncio 2' },
         { id: 3, name: 'Anúncio 3' },
         { id: 4, name: 'Anúncio 4' }
     ]);
+});
+
+app.get('/ads/:id/discord', (req, res) => {
+    // const adId = req.params.id;
+
+    return res.json([]);
 });
 
 app.listen(3333);
