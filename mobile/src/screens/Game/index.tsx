a3ef7c2efaec51a6430459e1eb10f9be00bef7fb
Angela Caldas <aes.caldas@gmail.com>
2022-09-16 20:17:49 -0300

    wip: game screen

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,12 +1,46 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Background } from '../../components/Background'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { TouchableOpacity, View, Image } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
 
 import { styles } from './styles'
 
+import logoImg from '../../assets/logo-nlw-esports.png'
+
+import { GameParams } from '../../@types/navigation'
+import { THEME } from '../../theme'
+import { Heading } from '../../components/Heading'
+
 export function Game() {
+    const route = useRoute()
+    const game = route.params as GameParams
+    const navigation = useNavigation()
+
+    function handleGoBack() {
+        navigation.goBack()
+    }
+
     return (
         <Background>
-            <SafeAreaView></SafeAreaView>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity onPress={handleGoBack}>
+                        <Entypo
+                            name='chevron-thin-left'
+                            color={THEME.COLORS.CAPTION_300}
+                            size={20}
+                        />
+                    </TouchableOpacity>
+                    <Image
+                        source={logoImg}
+                        style={styles.logo}
+                    />
+                    <View style={styles.right}></View>
+                </View>
+                <Image source={{ uri: game.bannerUrl }} style={styles.cover} resizeMode='cover' />
+                <Heading title={game.title} subtitle='Conecte-se e comece a jogar!' />
+            </SafeAreaView>
         </Background>
     )
 }
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -14,8 +14,8 @@ export function Home() {
     const [games, setGames] = useState<GameCardProps[]>([])
     const navigation = useNavigation()
 
-    function handleOpenGame() {
-        navigation.navigate('game')
+    function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+        navigation.navigate('game', { id, title, bannerUrl })
     }
 
     useEffect(() => {
@@ -41,7 +41,7 @@ export function Home() {
                     renderItem={({ item }) =>
                         <GameCard
                             data={item}
-                            onPress={handleOpenGame}
+                            onPress={() => handleOpenGame(item)}
                         />
                     }
                     horizontal
